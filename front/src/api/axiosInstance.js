24ccbcff1c3f727baaacf79e253e403f2fae56ff
priy2906 <priy.patel_ext@advaitbusiness.com>
2025-11-07 14:14:00 +0530

    api

--- front/src/api/axiosInstance.js
+++ front/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5001/api", // your backend base URL
+  baseURL: import.meta.env.VITE_API_BASE_URL, // your backend base URL
   headers: {
     "Content-Type": "application/json",
   },
