a2ac602128cdc9835542fc5c5789a4613f220627
priy2906 <patelpriy29@gmail.com>
2025-11-24 11:50:44 +0530

    fix

--- controllers/rentPaymentController.js
+++ controllers/rentPaymentController.js
@@ -1,55 +1,5 @@
 import RentPayment from "../models/RentPayment.js";
 
-// export const payMonthlyRent = async (req, res) => {
-//   try {
-//     const { rentId } = req.params;
-//     const { amountPaid } = req.body;
-
-//     const rent = await RentPayment.findById(rentId);
-
-//     if (!rent) {
-//       return res.status(404).json({
-//         success: false,
-//         message: "Rent record not found",
-//       });
-//     }
-
-//     // Check already paid
-//     if (rent.status === "paid") {
-//       return res.status(400).json({
-//         success: false,
-//         message: "Rent already paid",
-//       });
-//     }
-
-//     // Validate amount
-//     if (amountPaid !== rent.amount) {
-//       return res.status(400).json({
-//         success: false,
-//         message: `Amount mismatch. Expected ${rent.amount}, received ${amountPaid}`,
-//       });
-//     }
-
-//     // Mark as paid
-//     rent.status = "paid";
-//     rent.paidAt = new Date();
-
-//     await rent.save();
-
-//     return res.status(200).json({
-//       success: true,
-//       message: "Monthly rent paid successfully",
-//       data: rent,
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       success: false,
-//       message: "Failed to pay monthly rent",
-//       error: error.message,
-//     });
-//   }
-// };
-
 export const payMonthlyRent = async (req, res) => {
   try {
     const { rentId } = req.params;
