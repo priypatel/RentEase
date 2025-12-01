7b78a0ae258e8c9129e61ee4cda06c786dd1d29f
priy2906 <patelpriy29@gmail.com>
2025-12-01 17:56:05 +0530

    routes

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,175 +1,24 @@
-import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
-import "./index.css";
-
-// COMPONENT PAGES
-import TenantDashboardContent from "./pages/dashboard/TenantDashboardContent";
-import LandlordDashboardContent from "./pages/dashboard/LandlordDashboardContent";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import routes from "./routes";
 import ScrollToTop from "./components/ScrollToTop";
-import DashboardLayout from "./components/layout/DashboardLayout";
-import ProtectedRoute from "./components/ProtectedRoute";
 
-// COMMON PAGES
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import ResetPassword from "./pages/ResetPassword";
-import ForgotPassword from "./pages/ForgotPassword";
-import Home from "./pages/Home";
-
-// LANDLORD PAGES
-
-import AddProperty from "./pages/landlord/AddProperty";
-import Requests from "./pages/landlord/Requests";
-import MyProperties from "./pages/MyProperties";
-import ProfilePage from "./pages/ProfilePage";
-import PropertyDetailsPage from "./pages/PropertyDetailsPage";
-import Deposits from "./pages/landlord/Deposits";
-import LandlordPropertyPayments from "./pages/landlord/LandlordPropertyPayments";
-import LandlordPaymentHistory from "./pages/landlord/LandlordPaymentHistory";
-import TenantsPage from "./pages/landlord/TenantsPage";
-
-// TENANT PAGES
-
-import TenantPropertyDetails from "./pages/tenant/TenantPropertyDetails";
-import RentalStatusPage from "./pages/tenant/RentalStatusPage";
-import TenantPropertiesPage from "./pages/tenant/TenantPropertiesPage";
-import TenantDepositPage from "./pages/tenant/TenantDepositPage";
-import Payments from "./pages/tenant/Payments";
-import PaymentHistory from "./pages/tenant/PaymentHistory";
-import AvailableProperties from "./pages/tenant/AvailableProperties";
-
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <div className="bg-app min-h-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          {/* Public */}
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          {/* Password */}
-          <Route path="/reset-password/:token" element={<ResetPassword />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-
-          <Route
-            path="/"
-            element={
-              <ProtectedRoute>
-                <DashboardLayout />
-              </ProtectedRoute>
-            }
-          >
-            {/* Landlord Routes */}
-            <Route
-              path="landlord/dashboard"
-              element={<LandlordDashboardContent />}
-            />
-            <Route path="landlord/profile" element={<ProfilePage />} />
-            <Route
-              path="landlord/property/:id"
-              element={<PropertyDetailsPage />}
-            />
-            <Route path="/landlord/requests" element={<Requests />} />
-            <Route path="/landlord/deposits" element={<Deposits />} />
-            <Route
-              path="/landlord/payments"
-              element={<LandlordPropertyPayments />}
-            />
-
-            <Route
-              path="/landlord/payments/:id"
-              element={<LandlordPaymentHistory />}
-            />
-            <Route path="landlord/tenants" element={<TenantsPage />} />
-
-            {/* Common / Shared Routes */}
-            <Route path="my-properties" element={<MyProperties />} />
-            <Route path="add-property" element={<AddProperty />} />
-            <Route path="edit-property/:id" element={<AddProperty />} />
-          </Route>
-
-          {/* Tenant Routes (NO DASHBOARD LAYOUT) */}
-          <Route
-            path="/available-properties"
-            element={<AvailableProperties />}
-          />
-
-          {/* <Route
-            path="/tenant/available-properties"
-            element={
-              <ProtectedRoute>
-                <AvailableProperties />
-              </ProtectedRoute>
-            }
-          /> */}
-
-          <Route
-            path="/tenant/property/:id"
-            element={
-              <ProtectedRoute>
-                <TenantPropertyDetails />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/tenant/my-properties"
-            element={
-              <ProtectedRoute>
-                <TenantPropertiesPage />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/tenant/rental-status/:id"
-            element={
-              <ProtectedRoute>
-                <RentalStatusPage />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/tenant/payments"
-            element={
-              <ProtectedRoute>
-                <Payments />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/tenant/payments/:id"
-            element={
-              <ProtectedRoute>
-                <PaymentHistory />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/tenant/deposit/:id"
-            element={
-              <ProtectedRoute>
-                <TenantDepositPage />
-              </ProtectedRoute>
-            }
-          />
-
-          <Route
-            path="/tenant/profile"
-            element={
-              <ProtectedRoute>
-                <ProfilePage />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
+      <Routes>
+        {routes.map((route, i) =>
+          route.children ? (
+            <Route key={i} path={route.path} element={route.element}>
+              {route.children.map((child, idx) => (
+                <Route key={idx} path={child.path} element={child.element} />
+              ))}
+            </Route>
+          ) : (
+            <Route key={i} path={route.path} element={route.element} />
+          )
+        )}
+      </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
