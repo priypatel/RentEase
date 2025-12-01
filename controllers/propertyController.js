f37a9e1ea643cf76b82d6a6cd03e2f47bc0000bf
priy2906 <patelpriy29@gmail.com>
2025-12-01 16:44:37 +0530

    pagination

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -53,13 +53,28 @@ export const getProperties = async (req, res) => {
     if (req.originalUrl.includes("my-properties")) {
       query.ownerId = req.user.id;
     }
+    // Pagination values
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 20;
+    let skip = (page - 1) * limit;
 
+    const total = await Property.countDocuments(query);
     const properties = await Property.find(query)
       .populate("ownerId", "name email phone") // ⭐ Add this
-      .sort({ createdAt: -1 });
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
     // const properties = await Property.find(query).sort({ createdAt: -1 });
 
-    res.json({ properties });
+    res.json({
+      properties,
+      pagination: {
+        total,
+        page,
+        limit,
+        totalPages: Math.ceil(total / limit),
+      },
+    });
   } catch (err) {
     res.status(500).json({ message: "Failed to fetch properties" });
   }
@@ -96,12 +111,26 @@ export const searchProperties = async (req, res) => {
       if (minRent) filter.rent.$gte = Number(minRent);
       if (maxRent) filter.rent.$lte = Number(maxRent);
     }
+    // Pagination values
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 10;
+    let skip = (page - 1) * limit;
 
     const properties = await Property.find(filter)
       .populate("ownerId", "name email phone")
-      .sort({ createdAt: -1 });
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
 
-    res.json({ results: properties.length, properties });
+    res.json({
+      properties,
+      pagination: {
+        total: properties.length,
+        page: 1,
+        limit: properties.length,
+        totalPages: Math.ceil(properties.length / 10),
+      },
+    });
   } catch (err) {
     res.status(500).json({
       message: "Failed to search properties",
