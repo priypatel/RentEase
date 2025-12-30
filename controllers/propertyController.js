7f9bb15adf40a076f7fe12813fcf60152ebb8ddb
Priy Patel <patelpriy29@gmail.com>
2025-12-30 11:32:48 +0530

    fix image

--- controllers/propertyController.js
+++ controllers/propertyController.js
@@ -18,15 +18,10 @@ export const createProperty = async (req, res) => {
 
     if (req.files?.length > 0) {
       uploadedImages = req.files.map((file) => ({
-        // url:
-        //   process.env.USE_CLOUDINARY === "true"
-        //     ? file.path
-        //     : `${process.env.BASE_URL}/uploads/properties/${file.filename}`,
         url:
           process.env.USE_CLOUDINARY === "true"
             ? file.path
-            : `/uploads/properties/${file.filename}`,
-
+            : `${process.env.BASE_URL}/uploads/properties/${file.filename}`,
         public_id: file.filename,
       }));
     }
@@ -223,15 +218,10 @@ export const updateProperty = async (req, res) => {
     // ------------------------------
     if (req.files?.length > 0) {
       const newImages = req.files.map((file) => ({
-        // url:
-        //   process.env.USE_CLOUDINARY === "true"
-        //     ? file.path
-        //     : `${process.env.BASE_URL}/uploads/properties/${file.filename}`,
         url:
           process.env.USE_CLOUDINARY === "true"
             ? file.path
-            : `/uploads/properties/${file.filename}`,
-
+            : `${process.env.BASE_URL}/uploads/properties/${file.filename}`,
         public_id: file.filename,
       }));
 
